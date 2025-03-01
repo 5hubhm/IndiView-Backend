0fcc984829e71abe66c984f66a13b9b2a82142f6
shubham <shubhamkarwariya245@gmail.com>
2025-03-01 22:08:49 +0530

    used conditional checks for local and vercel for multer middleware

--- src/app.js
+++ src/app.js
@@ -11,8 +11,7 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "100mb" })); // For JSON requests
-app.use(express.urlencoded({ limit: "100mb", extended: true })); // For form data
+app.use(express.urlencoded({extended: true })); // For form data
 
 app.use(express.static("public"));
 
